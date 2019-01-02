6aafc64677f4425abd4cec31d185278239c30748
zzp0753 <527482508@qq.com>
2019-01-03 01:07:45 +0800

    optimization

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # fullScroller
-A plugin of Javascript that make the full-screen section scrolls.Demo [here](http://zzp0753.github.io/fullScroller)
+A plugin of Javascript that scrolls the full-screen section.
+Demo <a href="//zzp0753.github.io/fullScroller" target="_blank">here</a>
 
+Easy to use.
+u can use "full-screen" as class name of the full-screen-size element.
 
-Easy to use.u can use "full-screen" as class name of the full-screen-size element.
+使用方法：
+1.引入js文件；
+2.给全屏滚动板块添加<code>full-screen</code>的类名即可。
 
-~~Do not use <code>class="full-screen otherClassName"</code>, use <code>class="full-screen"</code>.~~
-
-<code>class="full-screen otherClassName"</code> is OK.
-
-Or u can edit the source code as u wish.
-
-Native Javascript Plugin.Doesn't rely on JQuery or other libs.
+兼容IE，不依赖其他Javascript库实现。
--- fullScroller.js
+++ fullScroller.js
@@ -1,145 +1,94 @@
 
+
+var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+var isFirefox = userAgent.indexOf("Firefox") > -1;
+
 var arr = document.getElementsByClassName('full-screen');
 for(var i=0;i<arr.length;i++){
-	arr[i].style.height = document.documentElement.clientHeight+'px';
-	arr[i].style.width = '100%';
+    arr[i].style.height = document.documentElement.clientHeight+'px';
+    arr[i].style.width = '100%';
 }
 
-function omw(event) {
+
+var op = function(event) {
+    event = event || window.event;
     //var direction = detail > 0 ? 'Up' : 'Down';
-    var step = document.documentElement.clientHeight            //可视区高度
-    var cur_top = Math.floor(document.documentElement.scrollTop);    //已滚的高度
+    var step = document.documentElement.clientHeight;         //可视区高度
+    if(isFirefox){
+        var cur_top = Math.floor(window.scrollY);//已滚的高度
+        var ed = event.detail;
+    } else{
+        var cur_top = Math.floor(document.documentElement.scrollTop);//已滚的高度
+        var ed = 0 - event.wheelDelta;
+    }
+
     
     //监听页面位置，通过cur_top可控制DOM行为
     
     
+    
     var direction = 0;
-    if(event.detail>0){
-    	direction = 1;
+
+    if(ed>0){
+        direction = 1;
     }
-    else if(event.detail<0){
-    	direction = -1;
+    else if(ed<0){
+        direction = -1;
     }
     var height = direction * step + cur_top; 
     var x_height = Math.round(height/step)*step;    //滚过整数倍的可视区大小
 
     var sc_index = cur_top;
     function getScroll_po(){
-    	// console.log("x_height:"+x_height+"  cur_top:"+cur_top+"  sc_index:"+sc_index)
-    	if(sc_index<x_height){
+        // console.log("x_height:"+x_height+"  cur_top:"+cur_top+"  sc_index:"+sc_index)
+        if(sc_index<x_height){
 
-    		if(sc_index+30>x_height){
-    			document.getElementsByTagName('html')[0].scrollTop = x_height;
-    			document.getElementsByTagName('body')[0].scrollTop = x_height;
-    		}
-    		else{
-    			document.getElementsByTagName('html')[0].scrollTop = sc_index;
-    			document.getElementsByTagName('body')[0].scrollTop = sc_index;
-    		}
-    	}
-    	sc_index = sc_index + 30;
-    	if(sc_index>x_height){
-    		console.log("stop Interval")
-    		clearInterval(pos_interval);
-    	}
+            if(sc_index+15>x_height){
+                document.getElementsByTagName('html')[0].scrollTop = x_height;
+                document.getElementsByTagName('body')[0].scrollTop = x_height;
+            }
+            else{
+                document.getElementsByTagName('html')[0].scrollTop = sc_index;
+                document.getElementsByTagName('body')[0].scrollTop = sc_index;
+            }
+        }
+        sc_index = sc_index + 15;
+        if(sc_index>x_height){
+            // console.log("stop Interval01")
+            clearInterval(pos_interval);
+        }
     }
     function getScroll_na(){
-    	// console.log("x_height:"+x_height+"  cur_top:"+cur_top+"  sc_index:"+sc_index)
-    	if(sc_index>x_height){
-    		if(sc_index-15<x_height){
-    			document.getElementsByTagName('html')[0].scrollTop = x_height;
-    			document.getElementsByTagName('body')[0].scrollTop = x_height;
-    		}
-    		else{
-    			document.getElementsByTagName('html')[0].scrollTop = sc_index;
-    			document.getElementsByTagName('body')[0].scrollTop = sc_index;
-    		}
-    		
-    	}
-    	sc_index = sc_index - 15;
-    	if(sc_index<x_height){
-    		console.log("stop Interval")
-    		clearInterval(nag_interval);
-    	}
+        // console.log("x_height:"+x_height+"  cur_top:"+cur_top+"  sc_index:"+sc_index)
+        if(sc_index>x_height){
+            if(sc_index-15<x_height){
+                document.getElementsByTagName('html')[0].scrollTop = x_height;
+                document.getElementsByTagName('body')[0].scrollTop = x_height;
+            }
+            else{
+                document.getElementsByTagName('html')[0].scrollTop = sc_index;
+                document.getElementsByTagName('body')[0].scrollTop = sc_index;
+            }
+            
+        }
+        sc_index = sc_index - 15;
+        if(sc_index<x_height){
+            // console.log("stop Interval02")
+            clearInterval(nag_interval);
+        }
     }
 
-    if(x_height > cur_top){    	
-    	var pos_interval = setInterval(getScroll_po,1);	
+    if(x_height > cur_top){     
+        var pos_interval = setInterval(getScroll_po,1); 
     }
     else if(x_height < cur_top){
-    	var nag_interval = setInterval(getScroll_na,1);
+        var nag_interval = setInterval(getScroll_na,1);
     }
     return false;  
 }
-		if(document.addEventListener){  
-    document.addEventListener('DOMMouseScroll',omw,false);  
+
+if(document.addEventListener){  
+    document.addEventListener('DOMMouseScroll',op ,false);  
 }//火狐  
-    // window.onmousewheel=document.onmousewheel=fn;//IE/Opera/Chrome  
-		document.documentElement.onmousewheel = function(event) {
-    //var direction = delta > 0 ? 'Up' : 'Down';
-    var step = document.documentElement.clientHeight            //可视区高度
-    var cur_top = Math.floor(window.scrollY);    //当前滚过的高度
-    
-    //监听页面位置，通过cur_top可控制DOM行为
-    
-    var direction = 0;
-    console.dir(event)
-    if(event.deltaY>0){
-    	direction = 1;
-    }
-    else if(event.deltaY<0){
-    	direction = -1;
-    }
-    var height = direction * step + cur_top; 
-    var x_height = Math.round(height/step)*step;    //滚过整数倍的可视区大小
-
-    var sc_index = cur_top;
-    function getScroll_po(){
-    	// console.log("x_height:"+x_height+"  cur_top:"+cur_top+"  sc_index:"+sc_index)
-    	if(sc_index<x_height){
-    		if(sc_index+14>x_height){
-    			document.getElementsByTagName('html')[0].scrollTop = x_height;
-    			document.getElementsByTagName('body')[0].scrollTop = x_height;
-    		}
-    		else{
-    			document.getElementsByTagName('html')[0].scrollTop = sc_index;
-    			document.getElementsByTagName('body')[0].scrollTop = sc_index;
-    		}
-    	}
-    	sc_index = sc_index + 14;
-    	if(sc_index>x_height){
-    		console.log("stop Interval")
-    		clearInterval(pos_interval);
-    	}
-    }
-    function getScroll_na(){
-    	// console.log("x_height:"+x_height+"  cur_top:"+cur_top+"  sc_index:"+sc_index)
-    	if(sc_index>x_height){
-    		if(sc_index-15<x_height){
-    			document.getElementsByTagName('html')[0].scrollTop = x_height;
-    			document.getElementsByTagName('body')[0].scrollTop = x_height;
-    		}
-    		else{
-    			document.getElementsByTagName('html')[0].scrollTop = sc_index;
-    			document.getElementsByTagName('body')[0].scrollTop = sc_index;
-    		}
-    		
-    	}
-    	sc_index = sc_index - 15;
-    	if(sc_index<x_height){
-    		console.log("stop Interval")
-    		clearInterval(nag_interval);
-    	}
-    }
-
-
-    if(x_height > cur_top){    	
-    	var pos_interval = setInterval(getScroll_po,1);	
-    }
-    else if(x_height < cur_top){
-    	var nag_interval = setInterval(getScroll_na,1);
-    }
-    // document.getElementsByTagName('html').stop().animate({ scrollTop: x_height }, 400);
-    // document.getElementsByTagName('body').stop().animate({ scrollTop: x_height }, 400);    
-    return false;  
-};
+// window.onmousewheel=document.onmousewheel=fn;//IE/Opera/Chrome  
+document.documentElement.onmousewheel = op;
